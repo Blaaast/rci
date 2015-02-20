6600054fa14c0a26584dafc3107c8622a36e35a2
Tom Coquereau <tom@thau.me>
2015-02-20 10:55:03 +0100

    Add first element

---
+++ js/fullscreenForm.js
@@ -0,0 +1,594 @@
+;( function( window ) {
+
+	'use strict';
+
+	var support = { animations : Modernizr.cssanimations },
+		animEndEventNames = { 'WebkitAnimation' : 'webkitAnimationEnd', 'OAnimation' : 'oAnimationEnd', 'msAnimation' : 'MSAnimationEnd', 'animation' : 'animationend' },
+		// animation end event name
+		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ];
+
+	/**
+	 * extend obj function
+	 */
+	function extend( a, b ) {
+		for( var key in b ) {
+			if( b.hasOwnProperty( key ) ) {
+				a[key] = b[key];
+			}
+		}
+		return a;
+	}
+
+	/**
+	 * createElement function
+	 * creates an element with tag = tag, className = opt.cName, innerHTML = opt.inner and appends it to opt.appendTo
+	 */
+	function createElement( tag, opt ) {
+		var el = document.createElement( tag )
+		if( opt ) {
+			if( opt.cName ) {
+				el.className = opt.cName;
+			}
+			if( opt.inner ) {
+				el.innerHTML = opt.inner;
+			}
+			if( opt.appendTo ) {
+				opt.appendTo.appendChild( el );
+			}
+		}
+		return el;
+	}
+
+	/**
+	 * FForm function
+	 */
+	function FForm( el, options ) {
+		this.el = el;
+		this.store = {};
+		this.options = extend( {}, this.options );
+  		extend( this.options, options );
+  		this._init();
+	}
+
+	/**
+	 * FForm options
+	 */
+	FForm.prototype.options = {
+		// show progress bar
+		ctrlProgress : true,
+		// show navigation dots
+		ctrlNavDots : true,
+		// show [current field]/[total fields] status
+		ctrlNavPosition : true,
+		// reached the review and submit step
+		onReview : function() { return false; }
+	};
+
+	/**
+	 * init function
+	 * initialize and cache some vars
+	 */
+	FForm.prototype._init = function() {
+		// the form element
+		this.formEl = this.el.querySelector( 'form' );
+
+		// list of fields
+		this.fieldsList = this.formEl.querySelector( 'ol.fs-fields' );
+
+		// current field position
+		this.current = 0;
+
+		// all fields
+		this.fields = [].slice.call( this.fieldsList.children );
+
+		// total fields
+		this.fieldsCount = this.fields.length;
+
+		// show first field
+		classie.add( this.fields[ this.current ], 'fs-current' );
+
+		// create/add controls
+		this._addControls();
+
+		// create/add messages
+		this._addErrorMsg();
+
+		// init events
+		this._initEvents();
+	};
+
+	/**
+	 * addControls function
+	 * create and insert the structure for the controls
+	 */
+	FForm.prototype._addControls = function() {
+		// main controls wrapper
+		this.ctrls = createElement( 'div', { cName : 'fs-controls', appendTo : this.el } );
+
+		// continue button (jump to next field)
+		this.ctrlContinue = createElement( 'button', { cName : 'fs-continue', inner : 'Continue', appendTo : this.ctrls } );
+		this._showCtrl( this.ctrlContinue );
+
+		// navigation dots
+		if( this.options.ctrlNavDots ) {
+			this.ctrlNav = createElement( 'nav', { cName : 'fs-nav-dots', appendTo : this.ctrls } );
+			var dots = '';
+			for( var i = 0; i < this.fieldsCount; ++i ) {
+				dots += i === this.current ? '<button class="fs-dot-current"></button>' : '<button disabled></button>';
+			}
+			this.ctrlNav.innerHTML = dots;
+			this._showCtrl( this.ctrlNav );
+			this.ctrlNavDots = [].slice.call( this.ctrlNav.children );
+		}
+
+		// field number status
+		if( this.options.ctrlNavPosition ) {
+			this.ctrlFldStatus = createElement( 'span', { cName : 'fs-numbers', appendTo : this.ctrls } );
+
+			// current field placeholder
+			this.ctrlFldStatusCurr = createElement( 'span', { cName : 'fs-number-current', inner : Number( this.current + 1 ) } );
+			this.ctrlFldStatus.appendChild( this.ctrlFldStatusCurr );
+
+			// total fields placeholder
+			this.ctrlFldStatusTotal = createElement( 'span', { cName : 'fs-number-total', inner : this.fieldsCount } );
+			this.ctrlFldStatus.appendChild( this.ctrlFldStatusTotal );
+			this._showCtrl( this.ctrlFldStatus );
+		}
+
+		// progress bar
+		if( this.options.ctrlProgress ) {
+			this.ctrlProgress = createElement( 'div', { cName : 'fs-progress', appendTo : this.ctrls } );
+			this._showCtrl( this.ctrlProgress );
+		}
+	}
+
+	/**
+	 * addErrorMsg function
+	 * create and insert the structure for the error message
+	 */
+	FForm.prototype._addErrorMsg = function() {
+		// error message
+		this.msgError = createElement( 'span', { cName : 'fs-message-error', appendTo : this.el } );
+	}
+
+	/**
+	 * init events
+	 */
+	FForm.prototype._initEvents = function() {
+		var self = this;
+
+		// show next field
+		this.ctrlContinue.addEventListener( 'click', function(ev) {
+			// Check if a condition is fullfiled before moving to the next field
+			self._nextField();
+		} );
+
+		// show initial button
+		document.getElementById('startButton').addEventListener( 'click', function() {
+			// Check if a condition is fullfiled before moving to the next field
+			self.ctrlContinue.click();
+		} );
+
+		// init range slider
+		document.getElementById('q4').addEventListener('input', function() {
+			// Check if a condition is fullfiled before moving to the next field
+			document.querySelector('#volume').innerHTML = this.value;
+		});
+
+		// navigation dots
+		if( this.options.ctrlNavDots ) {
+			this.ctrlNavDots.forEach( function( dot, pos ) {
+				dot.addEventListener( 'click', function() {
+					self._showField( pos );
+				} );
+			} );
+		}
+
+		// jump to next field without clicking the continue button (for fields/list items with the attribute "data-input-trigger")
+		this.fields.forEach( function( fld ) {
+			if( fld.hasAttribute( 'data-input-trigger' ) ) {
+				var input = fld.querySelector( 'input[type="radio"]' ) || /*fld.querySelector( '.cs-select' ) ||*/ fld.querySelector( 'select' ); // assuming only radio and select elements (TODO: exclude multiple selects)
+				if( !input ) return;
+
+				switch( input.tagName.toLowerCase() ) {
+					case 'select' :
+						input.addEventListener( 'change', function(ev) { self._fieldHandler(ev); } );
+						break;
+
+					case 'input' :
+						[].slice.call( fld.querySelectorAll( 'input[type="radio"]' ) ).forEach( function( inp ) {
+							inp.addEventListener( 'change', function(ev) { self._fieldHandler(ev); } );
+						} );
+						break;
+
+					/*
+					// for our custom select we would do something like:
+					case 'div' :
+						[].slice.call( fld.querySelectorAll( 'ul > li' ) ).forEach( function( inp ) {
+							inp.addEventListener( 'click', function(ev) { self._fieldHandler(); } );
+						} );
+						break;
+					*/
+				}
+			}
+		} );
+
+		// keyboard navigation events - jump to next field when pressing enter
+		document.addEventListener( 'keydown', function( ev ) {
+			if( !self.isLastStep && ev.target.tagName.toLowerCase() !== 'textarea' ) {
+				var keyCode = ev.keyCode || ev.which;
+				if( keyCode === 13 ) {
+					ev.preventDefault();
+					self._fieldHandler(ev);
+				}
+			}
+		} );
+	};
+
+	/**
+	 * nextField function
+	 * jumps to the next field
+	 */
+	FForm.prototype._nextField = function( backto ) {
+		if( this.isLastStep || !this._validade() || this.isAnimating ) {
+			return false;
+		}
+		this.isAnimating = true;
+
+		// check if on last step
+		this.isLastStep = this.current === this.fieldsCount - 1 && backto === undefined ? true : false;
+
+		// clear any previous error messages
+		this._clearError();
+
+		// current field
+		var currentFld = this.fields[ this.current ];
+
+		// save the navigation direction
+		this.navdir = backto !== undefined ? backto < this.current ? 'prev' : 'next' : 'next';
+
+		// update current field
+		this.current = backto !== undefined ? backto : this.current + 1;
+
+		if( backto === undefined ) {
+			// update progress bar (unless we navigate backwards)
+			this._progress();
+
+			// save farthest position so far
+			this.farthest = this.current;
+		}
+
+		// add class "fs-display-next" or "fs-display-prev" to the list of fields
+		classie.add( this.fieldsList, 'fs-display-' + this.navdir );
+
+		// remove class "fs-current" from current field and add it to the next one
+		// also add class "fs-show" to the next field and the class "fs-hide" to the current one
+		classie.remove( currentFld, 'fs-current' );
+		classie.add( currentFld, 'fs-hide' );
+
+		if( !this.isLastStep ) {
+			// update nav
+			this._updateNav();
+
+			// change the current field number/status
+			this._updateFieldNumber();
+
+			var nextField = this.fields[ this.current ];
+			classie.add( nextField, 'fs-current' );
+			classie.add( nextField, 'fs-show' );
+		}
+
+		// after animation ends remove added classes from fields
+		var self = this,
+			onEndAnimationFn = function( ev ) {
+				if( support.animations ) {
+					this.removeEventListener( animEndEventName, onEndAnimationFn );
+				}
+
+				classie.remove( self.fieldsList, 'fs-display-' + self.navdir );
+				classie.remove( currentFld, 'fs-hide' );
+
+				if( self.isLastStep ) {
+					// show the complete form and hide the controls
+					self._hideCtrl( self.ctrlNav );
+					self._hideCtrl( self.ctrlProgress );
+					self._hideCtrl( self.ctrlContinue );
+					self._hideCtrl( self.ctrlFldStatus );
+					// replace class fs-form-full with fs-form-overview
+					classie.remove( self.formEl, 'fs-form-full' );
+					classie.add( self.formEl, 'fs-form-overview' );
+					classie.add( self.formEl, 'fs-show' );
+					// callback
+					self.options.onReview();
+				}
+				else {
+					classie.remove( nextField, 'fs-show' );
+
+					if( self.options.ctrlNavPosition ) {
+						self.ctrlFldStatusCurr.innerHTML = self.ctrlFldStatusNew.innerHTML;
+						self.ctrlFldStatus.removeChild( self.ctrlFldStatusNew );
+						classie.remove( self.ctrlFldStatus, 'fs-show-' + self.navdir );
+					}
+				}
+				self.isAnimating = false;
+			};
+
+		if( support.animations ) {
+			if( this.navdir === 'next' ) {
+				if( this.isLastStep ) {
+					currentFld.querySelector( '.fs-anim-upper' ).addEventListener( animEndEventName, onEndAnimationFn );
+				}
+				else {
+					nextField.querySelector( '.fs-anim-lower' ).addEventListener( animEndEventName, onEndAnimationFn );
+				}
+			}
+			else {
+				nextField.querySelector( '.fs-anim-upper' ).addEventListener( animEndEventName, onEndAnimationFn );
+			}
+		}
+		else {
+			onEndAnimationFn();
+		}
+	}
+
+	/**
+	 * showField function
+	 * jumps to the field at position pos
+	 */
+	FForm.prototype._showField = function( pos ) {
+		if( pos === this.current || pos < 0 || pos > this.fieldsCount - 1 ) {
+			return false;
+		}
+		this._nextField( pos );
+	}
+
+	/**
+	 * updateFieldNumber function
+	 * changes the current field number
+	 */
+	FForm.prototype._updateFieldNumber = function() {
+		if( this.options.ctrlNavPosition ) {
+			// first, create next field number placeholder
+			this.ctrlFldStatusNew = document.createElement( 'span' );
+			this.ctrlFldStatusNew.className = 'fs-number-new';
+			this.ctrlFldStatusNew.innerHTML = Number( this.current + 1 );
+
+			// insert it in the DOM
+			this.ctrlFldStatus.appendChild( this.ctrlFldStatusNew );
+
+			// add class "fs-show-next" or "fs-show-prev" depending on the navigation direction
+			var self = this;
+			setTimeout( function() {
+				classie.add( self.ctrlFldStatus, self.navdir === 'next' ? 'fs-show-next' : 'fs-show-prev' );
+			}, 25 );
+		}
+	}
+
+	/**
+	 * progress function
+	 * updates the progress bar by setting its width
+	 */
+	FForm.prototype._progress = function() {
+		if( this.options.ctrlProgress ) {
+			this.ctrlProgress.style.width = this.current * ( 100 / this.fieldsCount ) + '%';
+		}
+	}
+
+	/**
+	 * updateNav function
+	 * updates the navigation dots
+	 */
+	FForm.prototype._updateNav = function() {
+		if( this.options.ctrlNavDots ) {
+			classie.remove( this.ctrlNav.querySelector( 'button.fs-dot-current' ), 'fs-dot-current' );
+			classie.add( this.ctrlNavDots[ this.current ], 'fs-dot-current' );
+			this.ctrlNavDots[ this.current ].disabled = false;
+		}
+	}
+
+	/**
+	 * showCtrl function
+	 * shows a control
+	 */
+	FForm.prototype._showCtrl = function( ctrl ) {
+		classie.add( ctrl, 'fs-show' );
+	}
+
+	/**
+	 * hideCtrl function
+	 * hides a control
+	 */
+	FForm.prototype._hideCtrl = function( ctrl ) {
+		classie.remove( ctrl, 'fs-show' );
+	}
+
+	// TODO: this is a very basic validation function. Only checks for required fields..
+	FForm.prototype._validade = function() {
+		var fld = this.fields[ this.current ],
+			input = fld.querySelector( 'input[required]' ) || fld.querySelector( 'textarea[required]' ) || fld.querySelector( 'select[required]' ),
+			error;
+
+		if( !input ) return true;
+
+		switch( input.tagName.toLowerCase() ) {
+			case 'input' :
+				if( input.type === 'radio' || input.type === 'checkbox' ) {
+					var checked = 0;
+					[].slice.call( fld.querySelectorAll( 'input[type="' + input.type + '"]' ) ).forEach( function( inp ) {
+						if( inp.checked ) {
+							++checked;
+						}
+					} );
+					if( !checked ) {
+						error = 'NOVAL';
+					}
+				}
+				else if( input.value === '' ) {
+					error = 'NOVAL';
+				}
+				break;
+
+			case 'select' :
+				// assuming here '' or '-1' only
+				if( input.value === '' || input.value === '-1' ) {
+					error = 'NOVAL';
+				}
+				break;
+
+			case 'textarea' :
+				if( input.value === '' ) {
+					error = 'NOVAL';
+				}
+				break;
+		}
+
+		if( error != undefined ) {
+			this._showError( error );
+			return false;
+		}
+
+		return true;
+	}
+
+	// TODO
+	FForm.prototype._showError = function( err ) {
+		var message = '';
+		switch( err ) {
+			case 'NOVAL' :
+				message = 'Veuillez remplir le champ avant de continuer.';
+				break;
+			case 'INVALIDEMAIL' :
+				message = 'Veuillez entrer une adresse email valide.';
+				break;
+			// ...
+		};
+		this.msgError.innerHTML = message;
+		this._showCtrl( this.msgError );
+	}
+
+	// clears/hides the current error message
+	FForm.prototype._clearError = function() {
+		this._hideCtrl( this.msgError );
+	}
+
+	// ADDED at each step, check if some conditions are not fullfiled
+	FForm.prototype._fieldHandler = function(ev) {
+		var currentFld = this.fields[ this.current ];
+		var substrId = ev.target.id.substr(0, 2);
+		var fieldValue = ev.target.value;
+
+		// Check if the loop is not supposed to stop
+		// This is really heavy...
+		if (substrId === 'q2' && (fieldValue === 'viande' || fieldValue === 'lait')) {
+			this._showMessage('Vous ne pouvez pas rapporter ce genre de produit sur le sol français.');
+			return;
+		}
+		if (substrId === 'q2' && fieldValue === 'legumes') {
+			var quantity = document.getElementById('quantityHint');
+			quantity.innerHTML = '20Kg maximum pour cette catégorie';
+			this.store.type = 'fruits et légumes';
+		}
+		if (substrId === 'q2' && fieldValue === 'peche') {
+			var quantity = document.getElementById('quantityHint');
+			quantity.innerHTML = '5Kg maximum pour cette catégorie';
+			this.store.type = 'produits de pêche';
+		}
+		if (substrId === 'q2' && fieldValue === 'epices') {
+			var quantity = document.getElementById('quantityHint');
+			quantity.innerHTML = '2Kg maximum pour cette catégorie';
+			this.store.type = 'épices';
+		}
+
+		// Check if q3 passes
+		if (substrId === 'q3') {
+			if (this.store.q2 === 'legumes' && fieldValue > 20) {
+				this._showMessage('Vous ne pouvez pas rapporter plus de 20Kg de fruits et légumes sur le sol français.');
+				return;
+			}
+			if (this.store.q2 === 'peche' && fieldValue > 5) {
+				this._showMessage('Vous ne pouvez pas rapporter plus de 5Kg de produits de pêches sur le sol français.');
+				return;
+			}
+			if (this.store.q2 === 'epices' && fieldValue > 2) {
+				this._showMessage('Vous ne pouvez pas rapporter plus de 2Kg d\'épices sur le sol français.');
+				return;
+			}
+			this.store.weight = fieldValue;
+		}
+
+		if (substrId === 'q4' && fieldValue === 'eu') {
+			this._showMessage('Vous pouvez rapporter ce produit sans payer aucune taxe.');
+			return;
+		}
+
+		if (substrId === 'q6' && this.store.q5 === 'avion') {
+			if (fieldValue < 430) {
+				this._showMessage('Vous pouvez rapporter ce produit sans payer aucune taxe.');
+			}
+			if (fieldValue > 430 && fieldValue < 700) {
+				this._showMessage('Vous devez payer entre 0 et 2,5% de frais de douanes + 20% TVA. Soit pour ' +
+				                  this.store.weight + 'Kg de ' + this.store.type +
+				                  ': <ul><li>entre 0&euro; et ' + (fieldValue * 0.025) +
+				                  '&euro; de frais de douanes</li><li>' + (fieldValue * 0.20) +
+				                  '&euro; de TVA</li></ul>');
+			}
+			if (fieldValue > 700) {
+				this._showMessage('Vous devez payer entre 0 et 17% de frais de douanes + 20% TVA. Soit pour ' +
+				                  this.store.weight + 'Kg de ' + this.store.type +
+				                  ': <ul><li>entre 0&euro; et ' + (fieldValue * 0.17) +
+				                  '&euro; de frais de douanes</li><li>' + (fieldValue * 0.20) +
+				                  '&euro; de TVA</li></ul>');
+			}
+
+			return;
+		}
+		if (substrId === 'q6' && this.store.q5 === 'route') {
+			if (fieldValue < 300) {
+				this._showMessage('Vous pouvez rapporter ce produit sans payer aucune taxe.');
+			}
+			if (fieldValue > 300 && fieldValue < 700) {
+				this._showMessage('Vous devez payer entre 0 et 2,5% de frais de douanes + 20% TVA. Soit pour ' +
+				                  this.store.weight + 'Kg de ' + this.store.type +
+				                  ': <ul><li>entre 0&euro; et ' + (fieldValue * 0.025) +
+				                  '&euro; de frais de douanes</li><li>' + (fieldValue * 0.20) +
+				                  '&euro; de TVA</li></ul>');
+			}
+			if (fieldValue > 700) {
+				this._showMessage('Vous devez payer entre 0 et 17% de frais de douanes + 20% TVA. Soit pour ' +
+				                  this.store.weight + 'Kg de ' + this.store.type +
+				                  ': <ul><li>entre 0&euro; et ' + (fieldValue * 0.17) +
+				                  '&euro; de frais de douanes</li><li>' + (fieldValue * 0.20) +
+				                  '&euro; de TVA</li></ul>');
+			}
+			return;
+		}
+
+		// If everything's fine, set value in the store
+		this.store[substrId] = fieldValue;
+
+		// If no condition are met, move on to the next field
+		this._nextField();
+	}
+
+	// Show message instead of going to the next field
+	FForm.prototype._showMessage = function(content) {
+		// Show popin
+		classie.add( document.getElementById('container'), 'end-of-path' );
+		classie.add( document.body, 'hide-it-yo' );
+
+		document.getElementById('end-modal').innerHTML += '<p class="header">Frais de douanes</p><p class="content"> ' + content + '</p><p class="footer clearfix"><a id="btn-stay" class="btn-stay">Annuler<a/><a href="index.html" class="btn-retry">Recommencer<a/><a id="btn-sav" class="btn-sav">Appel conseiller<a/></p>';
+
+		document.getElementById('btn-stay').addEventListener('click', function() {
+			document.getElementById('end-modal').innerHTML = '';
+			classie.remove( document.getElementById('container'), 'end-of-path' );
+			classie.remove( document.body, 'hide-it-yo' );
+		});
+		document.getElementById('btn-sav').addEventListener('click', function() {
+			document.getElementById('end-modal').innerHTML = '<p>Appel d\'un conseiller en cours...</p>'
+		});
+	}
+
+	// add to global namespace
+	window.FForm = FForm;
+
+})( window );
